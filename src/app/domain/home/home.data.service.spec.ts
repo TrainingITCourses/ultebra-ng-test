f9e41b7f20f6c2a147fced3fa2a9571c819feaf8
Alberto Basalo <albertobasalo@hotmail.com>
2021-05-24 20:19:15 +0200

    test: home api

--- src/app/domain/home/home.data.service.spec.ts
+++ src/app/domain/home/home.data.service.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { EMPTY } from 'rxjs';
+import { of } from 'rxjs';
 import { HomeDataService } from './home.data.service';
+import { Project } from './models/project';
+import { Status } from './models/status';
 
 // describe('Home.DataService', () => {
 //   let service: HomeDataService;
@@ -21,17 +23,62 @@ fdescribe('The Home Data Service', () => {
     let httpClientSpy: jasmine.SpyObj<HttpClient>;
     beforeEach(() => {
       httpClientSpy = jasmine.createSpyObj('HttpClient', {
-        get: EMPTY,
+        get: of([
+          {
+            id: 'mi-primer-proyecto',
+            title: 'Mi primer proyecto',
+            description:
+              'Este es un proyecto de prueba. Para ver de qué soy capaz.',
+            budget: 1000,
+            status: Status.InProgress,
+          },
+          {
+            id: 'el-super-proyecto',
+            title: 'El super proyecto',
+            description:
+              'Ahora ya me he lanzado y voy con más confianza y recursos.',
+            budget: 9000,
+            status: Status.InProgress,
+          },
+        ]),
       });
       sut = new HomeDataService(httpClientSpy);
     });
-    describe('WHEN called and subscribed', () => {
+    describe('WHEN called', () => {
       beforeEach(() => {
         sut.getProjects$().subscribe();
       });
       it('THEN should use the expected url', () => {
         const actual = httpClientSpy.get.calls.mostRecent().args[0];
         const expected = 'https://api-base-21.herokuapp.com/api/pub/projects';
+        expect(actual).toEqual(expected);
+      });
+    });
+    describe('WHEN receives data ', () => {
+      beforeEach(() => {});
+      it('THEN should return the expected data', () => {
+        let actual: Project[] = [];
+        sut.getProjects$().subscribe({
+          next: (data) => (actual = data),
+        });
+        const expected: Project[] = [
+          {
+            id: 'mi-primer-proyecto',
+            title: 'Mi primer proyecto',
+            description:
+              'Este es un proyecto de prueba. Para ver de qué soy capaz.',
+            budget: 1000,
+            status: Status.InProgress,
+          },
+          {
+            id: 'el-super-proyecto',
+            title: 'El super proyecto',
+            description:
+              'Ahora ya me he lanzado y voy con más confianza y recursos.',
+            budget: 9000,
+            status: Status.InProgress,
+          },
+        ];
         expect(actual).toEqual(expected);
       });
     });
