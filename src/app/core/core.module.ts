fe77010ec32c3d39a48f4f785f9da40c70e253f7
Alberto Basalo <albertobasalo@hotmail.com>
2021-05-24 17:20:45 +0200

    feat: ng bse generations

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { DataModule } from '../shared/data/data.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, DataModule],
+})
+export class CoreModule {}
